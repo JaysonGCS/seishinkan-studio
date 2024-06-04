80134282ee9d4384eb41645ea7049c3978339cc9
“JaysonGCS“ <goh.chung.sern@gmail.com>
2024-06-05 01:32:29 +0800

    fix: :children_crossing: Improve loading page

--- src/app/_components/LoadingPage/LoadingPage.tsx
+++ src/app/_components/LoadingPage/LoadingPage.tsx
@@ -4,9 +4,9 @@ import { LoadingSkeleton } from '../LoadingSkeleton/LoadingSkeleton';
 
 export const LoadingPage = () => {
   return (
-    <main className="flex justify-center">
+    <main className="flex min-h-[calc(100vh-theme(height.navigation-menu-mobile))] justify-center bg-card pb-[theme(height.footer)] md:min-h-[calc(100vh-theme(height.navigation-menu-md))] lg:min-h-[calc(100vh-theme(height.navigation-menu-large))]">
       <div className="w-5/6 py-5 lg:w-6/12">
-        <LoadingSkeleton count={10} />
+        <LoadingSkeleton count={20} />
       </div>
     </main>
   );
